d5e5033885637fd6abbdc7fd4fe7f1e7c3ec7eb5
NoahMcGraw <nmcgr1996@hotmail.com>
2023-07-09 09:56:15 -0500

    enhance: updated readme to reflect new keys and updated openai to use chat/completions endpoint

--- src/services/openai/openai.ts
+++ src/services/openai/openai.ts
@@ -2,6 +2,16 @@
 import { Configuration, OpenAIApi } from 'openai'
 import SourceWord from '../../models/SourceWord.model'
 
+type getChatCompletionsGPT35TurboProps = {
+  systemPrompt: string
+  userPrompt: string
+  max_tokens?: number
+  temperature?: number
+  top_p?: number
+  presence_penalty?: number
+  frequency_penalty?: number
+}
+
 type getCompletionsProps = {
   prompt: string
   max_tokens?: number
@@ -25,17 +35,16 @@ type getSourceWordsProps = {
 
 export const getSourceWords = ({ wordNumber, topic, lang = 'en' }: getSourceWordsProps) =>
   new Promise<SourceWord[]>((resolve, reject) => {
-    getCompletionsDavinci003({
-      prompt: `
-        Topic:  ${topic},
-        Language Code: ${lang},
-        Instruction: Given the topic and language code, return one related phrase, noun, verb, or adjective to the topic.
-        API Response:`,
-      temperature: 0.75,
-      n: wordNumber,
+    getChatCompletionsGPT35Turbo({
+      systemPrompt:
+        "You are a function and your purpose is to take a topic, a languageCode, and a number of return values and return an array of the requested length containing related nouns, verbs, or adjectives to the topic in the language specified.\n\nYou can receive one JSON object as a argument containing 3 properties: \ntopic: string - The word to generate related words around, languageCode: string -\n The shortcode for the topic and related words' source language. \nn: number: The number of related words to generate.\n\nGiven the topic and language code, your function is to return a JSON object containing 1 property: \ndata: string[] - The array of related nouns, verbs, or adjectives\n\nIf one or more of the properties in the argument object were not passed, you should return a JSON object containing 2 properties:\nstatus: number - Http status code of bad params (400)\nerror: string - Message containing the reason for error.",
+      userPrompt: `{\n"topic": ${topic},\n"languageCode": ${lang},\n"n": ${wordNumber}\n}`,
+      frequency_penalty: 2,
+      presence_penalty: 2,
+      max_tokens: 3000,
     })
       .then((sourceWordArr: string[]) => {
-        console.log('Source Words:', sourceWordArr)
+        // console.log('Source Words:', sourceWordArr)
         resolve(sourceWordArr)
       })
       .catch((error) => {
@@ -101,6 +110,72 @@ export const getCompletionsDavinci003 = async ({
       const error = _error as Error
       console.error('Error parsing completions:', error.message)
     }
+  } catch (_error) {
+    const error = _error as Error
+    throw new Error(error.message)
+  }
+
+  return response
+}
+
+export const getChatCompletionsGPT35Turbo = async ({
+  systemPrompt,
+  userPrompt,
+  max_tokens = 256,
+  temperature = 1,
+  top_p = 1,
+  presence_penalty = 1,
+  frequency_penalty = 1,
+}: getChatCompletionsGPT35TurboProps): Promise<string[]> => {
+  // OpenAI API configuration
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.VITE_OPENAI_API_KEY,
+    })
+  )
+
+  // Hard token limit for OpenAI API under text-davinci-003 model
+  const openaiHardTokenLimit = 4000
+
+  let response = [] as string[]
+
+  try {
+    //check if prompt exceeds out max soft token limit
+    // if (!isWithinTokenLimit(prompt, openaiHardTokenLimit)) {
+    //   throw new Error('Error creating prompt: Prompt exceeds max soft token limit')
+    // }
+
+    const responseObj = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: systemPrompt,
+        },
+        {
+          role: 'user',
+          content: userPrompt,
+        },
+      ],
+      temperature,
+      max_tokens,
+      top_p,
+      frequency_penalty,
+      presence_penalty,
+    })
+    console.log('Response:', responseObj.data.choices[0].message)
+
+    // const completionsStr = responseObj.data.choices[0]?.text
+    // console.log('Completions:', completionsStr)
+    // Parse the completions string into an array and trim out any non-standard characters
+    // try {
+    //   if (typeof completionsStr !== 'undefined') {
+    //     response = parseCompletions(completionsStr)
+    //   }
+    // } catch (_error) {
+    //   const error = _error as Error
+    //   console.error('Error parsing completions:', error.message)
+    // }
   } catch (_error) {
     const error = _error as Error
     throw new Error(error.message)
