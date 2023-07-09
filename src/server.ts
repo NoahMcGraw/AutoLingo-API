76dbcb1515b34b702125da6fbb1439ca78e363fe
Noah McGraw <noahmcgraw@unisyntechnologies.com>
2023-07-09 15:51:16 -0500

    feat: deployed to gcloud

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-import https from 'https'
-import http from 'http'
-import fs from 'fs'
+// import https from 'https'
+// import http from 'http'
+// import fs from 'fs'
 import dotenv from 'dotenv'
 import { buildTranslationsList } from './services/integration-bridge/bridge'
 import { getSearchSuggestions } from './services/datamuse/datamuse'
@@ -20,32 +20,36 @@ app.use(cors())
 // Enable origin checker middleware
 app.use(originChecker)
 
-const httpsPort = process.env.VITE_HTTPS_PORT || 443
-const httpPort = process.env.VITE_HTTP_PORT || 80
+// const httpsPort = process.env.HTTPS_PORT || 443
+const httpPort = process.env.PORT || 80
 
-// Create HTTPS server
-https
-  .createServer(
-    {
-      // Grab paths to SSL cert and private key from environment variables
-      cert: fs.readFileSync(`${process.env.VITE_PATH_TO_SSL_CERT}`),
-      key: fs.readFileSync(`${process.env.VITE_PATH_TO_SSL_KEY}`),
-    },
-    app
-  )
-  .listen(httpsPort, () => {
-    console.log(`Https Server is listening on port ${httpsPort}.`)
-  })
+// // Create HTTPS server
+// https
+//   .createServer(
+//     {
+//       // Grab paths to SSL cert and private key from environment variables
+//       cert: fs.readFileSync(`${process.env.VITE_PATH_TO_SSL_CERT}`),
+//       key: fs.readFileSync(`${process.env.VITE_PATH_TO_SSL_KEY}`),
+//     },
+//     app
+//   )
+//   .listen(httpsPort, () => {
+//     console.log(`Https Server is listening on port ${httpsPort}.`)
+//   })
 
-// Create HTTP server to redirect all HTTP requests to HTTPS
-http
-  .createServer((req, res) => {
-    res.writeHead(301, { Location: 'https://' + req.headers['host'] + req.url })
-    res.end()
-  })
-  .listen(httpPort, () => {
-    console.log(`Http Server is listening on port ${httpPort}.`)
-  })
+// // Create HTTP server to redirect all HTTP requests to HTTPS
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(301, { Location: 'https://' + req.headers['host'] + req.url })
+//     res.end()
+//   })
+//   .listen(httpPort, () => {
+//     console.log(`Http Server is listening on port ${httpPort}.`)
+//   })
+
+app.listen(httpPort, () => {
+  console.log(`Http Server is listening on port ${httpPort}.`)
+})
 
 // Provide a basic endpoint for testing
 app.get('/', (_req, res) => {
